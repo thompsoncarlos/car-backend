20d3b72270681925368f55f0b561a5b7b1a9b428
carlosthompson <carlosethompson@gmail.com>
2026-03-23 12:27:24 +0000

    feat: update class

--- src/main/java/car_backend/repository/reports/ReportZ05Repository.java
+++ src/main/java/car_backend/repository/reports/ReportZ05Repository.java
@@ -1,32 +1,29 @@
 package car_backend.repository.reports;
 
 import car_backend.model.reports.ReportZ05;
+import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+@Repository
 public interface ReportZ05Repository extends JpaRepository<ReportZ05, String> {
-    @Query(value = """
-      SELECT DISTINCT
-          a.ACTIVITY_ID,
-          COALESCE(es.EBA_SERVICES_ID, '#UNV'),
-          CASE
-              WHEN a.ACTIVITY_LABEL IS NOT NULL
-              THEN CONCAT(CONCAT(a.ACTIVITY_ID, '_'), a.ACTIVITY_LABEL)
-              ELSE CONCAT(a.ACTIVITY_ID, '_#UNV')
-          END,
-          COALESCE(ebl.CORE_BUSINESS_LINE_NAME, '#UNV'),
-          COALESCE(ebl.CORE_BUSINESS_LINE_ID, '#UNV')
-      FROM
-          PHI_T_ACTIVITY a
-          LEFT OUTER JOIN PHI_T_ACTIVITY_TYPE_CATEGORY atc
-              ON a.ACTIVITY_TYPE_ID = atc.ACTIVITY_TYPE_ID
-          LEFT OUTER JOIN PHI_T_REL_EBA_SERVICES_ACTIVITY_TYPE resat
-              ON atc.ACTIVITY_TYPE_ID = resat.ACTIVITY_TYPE_ID
-          LEFT OUTER JOIN PHI_T_EBA_SERVICES es
-              ON resat.EBA_SERVICES_ID = es.EBA_SERVICES_ID
-          LEFT OUTER JOIN phi_t_eba_business_line ebl
-              ON ebl.core_business_line_id = atc.core_business_line_id
-        """, nativeQuery = true)
-    List<Object[]> generateReportData();
+    
+    @Modifying
+    @Transactional
+    @Query(value = "TRUNCATE TABLE PHI_T_REPORT_Z05", nativeQuery = true)
+    void truncateReportTable();
+
+    @Modifying
+    @Transactional
+    @Query(value = "SELECT", nativeQuery = true)
+    void insertReportTable();
+
+    @Modifying
+    @Transactional
+    @Query(value = "CALL P_REPORT_Z05.MAIN_Z05()", nativeQuery = true)
+    void executeReportZ05();
+
+}
--- src/main/java/car_backend/service/reports/ReportZ05ServiceImpl.java
+++ src/main/java/car_backend/service/reports/ReportZ05ServiceImpl.java
@@ -1,17 +1,16 @@
 
 package car_backend.service.reports;
 
-import car_backend.model.reports.ReportZ01;
 import car_backend.model.reports.ReportZ05;
-import car_backend.repository.relations.ActivityPersonRelRepository;
-import car_backend.repository.reports.ReportZ01Repository;
+import car_backend.repository.reports.ReportZ05Repository;
+import car_backend.utils.reports.ReportsExcelBuilder;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
+import org.apache.poi.ss.usermodel.Row;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 import java.util.List;
 
 @Slf4j
@@ -19,22 +18,37 @@ import java.util.List;
 public class ReportZ05ServiceImpl implements ReportZ05Service {
 
     @Autowired
-    private ReportZ01Repository z01Repository;
+    private ReportZ05Repository reportZ05Repository;
 
     @Autowired
-    private ActivityPersonRelRepository activityPersonRelRepository;
+    ReportsExcelBuilder excelBuilder;
 
     @Override
-    public List<ReportZ05> generateReportZ05() {
-
-        List<ReportZ05> output = new ArrayList<>();
-
-        List<ReportZ01> z01List = z01Repository.findAll();
-        if (z01List.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+    public byte[] generateZ05ReportExcel() {
+        try {
+            List<ReportZ05> reports = getReport();
+            return excelBuilder.buildGenericReport(reports, "templates/report_z05.xlsx", 15, this::writeReportZ05);
+        } catch (IOException e) {
+            log.error("Error generating report z05 excel", e);
+            throw new RunTimeException("Failed to generate z05 report", e);
         }
-        List<String> activities = z01List.stream().map(ReportZ01::getServiceId).toList();
-
-        return List.of();
     }
+    
+    private List<ReportZ05> getReport() {
+        reportZ05Repository.executeReportZ05();
+        return reportZ05Repository.findAll();
+    }
+
+    private void writeReportZ05(ReportZ05 reportZ05, Row row) {
+        int cellCount = 1;
+
+        excelBuilder.fillCellWithString(reportZ05.getServiceId(), row, cellCount++); // SERVICE_IDENTIFIER_0005
+        excelBuilder.fillCellWithString(reportZ05.getServiceType(), row, cellCount++); // SERVICE_TYPE_0010
+        excelBuilder.fillCellWithString(reportZ05.getServiceUniqueLabel(), row, cellCount++); // UNIQUE_SERVICE_TITLE_BK_TAXO_0020
+        excelBuilder.fillCellWithString(reportZ05.getCoreBusinessLineName(), row, cellCount++); // CORE_BUSINESS_LINE_NAME_0030
+        excelBuilder.fillCellWithString(reportZ05.getCoreBusinessLineId(), row, cellCount++); // CORE_BUSINESS_LINE_ID_0040
+
+    }
+
+
 }
